e001cb5e0c99d33291200fb62289a0d94bd0f595
Petemab <peteben18@hotmail.com>
2018-07-14 13:44:32 +0100

    map functioning. Tryig to get centering to work

--- WeatherApp/src/common/Map.js
+++ WeatherApp/src/common/Map.js
@@ -14,22 +14,15 @@ class Map extends React.Component {
 
   componentDidMount() {
     this.map = new google.maps.Map(this.mapDiv, {
-      // center: this.props.center,
-      zoom: 14
+      center: this.props.center,
+      zoom: 5
     });
-    //
-    // this.marker = new google.maps.Marker({
-    //   position: this.map.getCenter(),
-    //   map: this.map,
-    //   label: '😆'
-    // });
 
-    // console.log('Pub Marker', this.props.pubMarker);
-    // this.pubMarker = new google.maps.Marker({
-    //   position: this.props.pubMarker,
-    //   map: this.map,
-    //   label: '🍺'
-    // });
+    this.marker = new google.maps.Marker({
+      position: this.map.getCenter(),
+      map: this.map
+    });
+
 
 
 
@@ -37,18 +30,20 @@ class Map extends React.Component {
 
 
 
-  //to make sure map is destroyed after leaving page
-  // componentWillUnmount() {
-  //   this.markers.forEach(marker => marker.setMap(null));
-  //   this.markers = null;
-  //   this.marker.setMap(null);
-  //   this.pubMarker = null;
-  //   this.map = null;
-  // }
+  // to make sure map is destroyed after leaving page
+  componentWillUnmount() {
+    this.markers.forEach(marker => marker.setMap(null));
+    this.markers = null;
+    this.marker.setMap(null);
+    this.pubMarker = null;
+    this.map = null;
+  }
 
   render() {
+
     const className = this.props.className ? this.props.className + ' map' : 'map';
     return (
+
       <div className={className} ref={el => this.mapDiv = el}/>
     );
   }
--- WeatherApp/src/components/Home.js
+++ WeatherApp/src/components/Home.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import Map from '../common/Map';
+import AutoComplete from '../common/AutoComplete';
 
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Check Your Weather Below</h1>
-      <form>
-        <Map className="map"/>
-      </form>
-    </div>
+class Home extends React.Component{
+
+  handleChange = ({ target: { name, value } }) => {
+    const errors = {...this.state.errors, [name]: '' };
+    this.setState({ errors, [name]: value });
+
+  }
+
+  handlePlaceChange = ({ formatted_address: address, geometry: { location
+  } }) => {
+    this.setState({ address, location: location.toJSON() }, () =>
+      console.log(this.state.location.lat, this.state.location.lng));
+  }
+
+  render() {
+    // const { location } = this.state.location.lat;
+    // console.log('render', location);
+    return (
+      <div>
+        <h1>Check Your Weather Forecast Below</h1>
+        <div className="field">
+          <label htmlFor="address">Address</label>
+          <AutoComplete id="address" name="address" className="input"
+            placeholder="Address" handlePlaceChange={this.handlePlaceChange} />
+        </div>
+
+        <Map className="map" center={{ lat: 51.5074, lng: 0.1278}}/>
+
+      </div>
 
 
-  );
+    );
 
-
-};
+  }
+}
 
 export default Home;
